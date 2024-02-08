45162998f49d230489760560a901eb4bbee63c1a
Claudia Villarroel <claudia.villarroel@live.com>
2024-02-08 16:39:23 -0300

    :hammer: Primitiva effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import styles from './App.module.css';
-import { createSignal } from 'solid-js';
+import { createSignal, createEffect } from 'solid-js';
 
 function App() {
   const [count, setCount] = createSignal(0);
+  const [count2, setCount2] = createSignal(0);
+
+  createEffect(() => {
+    console.log('Count: ' + count());
+    console.log('Count2: ' + count2());
+  });
+
+  createEffect(() => {
+    localStorage.setItem('count', count());
+  });
   return (
     <div class={styles.App}>
       <h1>Count: {count()}</h1>
       <button onClick={() => setCount(count() + 1)}>{count()}</button>
+      <button onClick={() => setCount2(count2() + 1)}>{count2()}</button>
     </div>
   );
 }
